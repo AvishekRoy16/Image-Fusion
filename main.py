a977fd256e43baa2b1074b2d7c9aff482a2a13ae
adarshcode12 <97828274+adarshcode12@users.noreply.github.com>
2022-10-28 23:58:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-from pose_estimator import PoseEstimator
-import custom_shoulder_locator
-from segmentation import cloth_extractor
-import utils
-
 import cv2 as cv
 import imutils
 import logging
 
-#TODO: create error list
-ERROR_LIST = [] # e.g. shoulder detection error
-MIN_SHOULDER_DISTANCE=0
-
-class CMate:
-    def __init__(self, source_img, dest_img):
-        self.source_img = source_img
-        self.dest_img = dest_img
-        self.dest_pose_estimator = PoseEstimator(cv.imread(self.dest_img))
-        self.source_pose_estimator = None  # initialize after cloth extraction
-        self.error_list = []
 
 def find_rotation_angle(a, b):
     """
