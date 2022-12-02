09aa94339ad3dc543e9b75d6c70686aa81ad56cd
adarshcode12 <adarshcode12@gmail.com>
2022-12-03 00:22:46 +0530

    IF-28 pose_detection function added

--- pose_detection.py
+++ pose_detection.py
@@ -1,50 +1,129 @@
-# pip install mediapipe
+"""
+pose estimator for extrating shoulder location.
+"""
+import cv2 as cv
+import math
+import os
 
-import cv2
-import mediapipe as mp
-import matplotlib.pyplot as plt
+# choose one of "mpi" or "body_24" openpose model 
+model = "body_25"
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+MODEL_DIR = os.path.join(BASE_DIR, "segmentation", "models")
 
-# Initialize mediapipe pose class.
-mp_pose = mp.solutions.pose
+if model == "mpi":
+    protoFile = os.path.join(MODEL_DIR, "pose_deploy_linevec.prototxt")
+    weightsFile = os.path.join(MODEL_DIR, "pose_iter_160000.caffemodel")
+    THRESHOLD = 0.3
+else:
+    protoFile = os.path.join(MODEL_DIR, "mpii_openpose_body25", "pose_deploy.prototxt")
+    weightsFile = os.path.join(MODEL_DIR, "mpii_openpose_body25", "pose_iter_584000.caffemodel")
+    THRESHOLD = 0.1
 
-# Setup the Pose function for images - independently for the images standalone processing.
-pose_image = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.5)
+HEIGHT = 368
+WIDTH = 368
+SCALE = 0.003922  # 1.0/255
 
-# Setup the Pose function for videos - for video processing.
-pose_video = mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.7,
-                          min_tracking_confidence=0.7)
+# body parts ordering
+# https://github.com/CMU-Perceptual-Computing-Lab/openpose/blob/master/doc/output.md#keypoint-ordering-in-cpython
+BODY_PARTS = {"RShoulder": 2, "LShoulder": 5}
 
-# Initialize mediapipe drawing class - to draw the landmarks points.
-mp_drawing = mp.solutions.drawing_utils
 
-def detectPose(image_pose, pose, draw=False, display=False):
-    
-    original_image = image_pose.copy()
-    
-    image_in_RGB = cv2.cvtColor(image_pose, cv2.COLOR_BGR2RGB)
-    
-    resultant = pose.process(image_in_RGB)
-    
-    if resultant.pose_landmarks and draw:
-    
-        mp_drawing.draw_landmarks(image=original_image, landmark_list=resultant.pose_landmarks,
-                                  connections=mp_pose.POSE_CONNECTIONS,
-                                  landmark_drawing_spec=mp_drawing.DrawingSpec(color=(255,255,255),
-                                                                               thickness=3, circle_radius=3),
-                                  connection_drawing_spec=mp_drawing.DrawingSpec(color=(49,125,237),
-                                                                               thickness=2, circle_radius=2))
+def find_rotation_angle(a, b):
+    """
+    find angle a of right angled traingle with ab as hypotenous
+    <)bac
+    """
+    try:
+        c = (b[0], a[1])
+        ratio = (c[1]-b[1])/(c[0]-a[0])
+        # print("ratio",ratio)
+        angle = math.degrees(math.atan(ratio))
+        return angle
+    except ZeroDivisionError:
+        raise Exception("left shoulder and right shoulder detected at same location.")
 
-    if display:
 
-        plt.figure(figsize=[22,22])
-        plt.subplot(121);plt.imshow(image_pose[:,:,::-1]);plt.title("Input Image");plt.axis('off');
-        plt.subplot(122);plt.imshow(original_image[:,:,::-1]);plt.title("Pose detected Image");plt.axis('off');
+class PoseEstimator:
+    def __init__(self, frame):
+        """
+        Initialize estimator
+        frame: image array
+        """
+        self.frame = frame
+        self.net = cv.dnn.readNetFromCaffe(protoFile, weightsFile)
+        self.shoulder_points = []
 
-    else:
+    def get_shoulder_points(self):
+        """
+        return shoulder locations.
+        """
+        if len(self.shoulder_points) > 0:
+            return self.shoulder_points
 
-        return original_image, results
+        frameWidth = self.frame.shape[1]
+        frameHeight = self.frame.shape[0]
+        inp = cv.dnn.blobFromImage(self.frame, SCALE, (WIDTH, HEIGHT),
+                                   (0, 0, 0), swapRB=False, crop=False)
+        self.net.setInput(inp)
+        self.out = self.net.forward()
+        # print(self.out.shape)
 
-# Here we will read our image from the specified path to detect the pose
-image_path = 'sample.jpg'
-output = cv2.imread(image_path)
-detectPose(output, pose_image, draw=True, display=True)
+        assert(len(BODY_PARTS) <= self.out.shape[1])
+
+        # find right and left shoulder
+        shoulder_points = []
+        for i in [2, 5]:
+            # Slice heatmap of corresponding body's part.
+            heatMap = self.out[0, i, :, :]
+
+            _, conf, _, point = cv.minMaxLoc(
+                heatMap)  # (min, max, minloc, maxloc)
+            x = (frameWidth * point[0]) / self.out.shape[3]
+            y = (frameHeight * point[1]) / self.out.shape[2]
+
+            # Add a point if it's confidence is higher than threshold.
+            if conf > THRESHOLD:
+                shoulder_points.append((int(x), int(y)))
+
+        print("Shoulder Points:", shoulder_points)
+        return shoulder_points
+
+    def get_shoulder_details(self):
+        # get shoulder points
+        self.shoulder_points = self.get_shoulder_points()
+        # step 3: get shoulder width and rotation angle
+        # raises: shoulder not found.
+        if len(self.shoulder_points) < 2:
+            raise Exception("image without shoulder.")
+
+        distance = self.shoulder_points[1][0] - self.shoulder_points[0][0]
+        # rotation_angle = find_rotation_angle(self.shoulder_points[0], self.shoulder_points[1])
+
+        return distance
+
+    def visualize_pose(self):
+        try:
+            self.shoulder_points = self.get_shoulder_points()
+            
+            import matplotlib.pyplot as plt
+            plt.figure(figsize=(10,5))
+            # draw points
+            plt.subplot(1,2,1)
+            cv.ellipse(self.frame, tuple(self.shoulder_points[0]),
+                       (3, 3), 0, 0, 360, (0, 255, 0), cv.FILLED)
+            cv.ellipse(self.frame, tuple(self.shoulder_points[1]),
+                       (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
+            plt.imshow(cv.cvtColor(self.frame, cv.COLOR_BGR2RGB))
+            # draw heatmaps
+            plt.subplot(1, 2, 2)
+            plt.imshow(cv.cvtColor(self.frame, cv.COLOR_BGR2RGB))          
+            for pt in [2, 5]:
+                probMap = self.out[0, pt, :, :]
+                probMap = cv.resize(probMap,(self.frame.shape[1], self.frame.shape[0]))
+                
+                plt.imshow(probMap, alpha=0.6)
+            plt.colorbar()
+            plt.axis("off")
+            plt.show()
+        except IndexError:
+            raise Exception("Shoulder not detected.")
